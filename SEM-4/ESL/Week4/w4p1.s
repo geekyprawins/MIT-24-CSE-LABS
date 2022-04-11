4934aed91e645e5988b06184f27a75aa1ef5e3f5
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-04-11 19:04:27 +0530

    ESL Week4

---
+++ SEM-4/ESL/Week4/w4p1.s
@@ -0,0 +1,37 @@
+;32-bit packed bcd to hex format
+	AREA RESET, DATA, READONLY
+	EXPORT __Vectors
+__Vectors
+	DCD 0X10001000
+	DCD Reset_Handler
+	ALIGN
+	AREA mycode, CODE, READONLY
+	ENTRY
+	EXPORT Reset_Handler
+	
+Reset_Handler
+
+	LDR R0, PACKED
+	LDR R1, =DST
+	MOV R2, #10
+	MOV R3, #1
+	MOV R5, #0
+	
+LOOP
+	AND R4, R0, #0X0000000F
+	MLA R5, R4, R3, R5
+	MUL R3, R3, R2
+	LSR R0, #4
+	CMP R0, #0
+	BNE LOOP
+	
+	STR R5, [R1]
+	
+HERE
+	B HERE
+
+PACKED	DCD 0X999
+	
+	AREA mydata, DATA, READWRITE
+DST	DCD 0
+	END
---
+++ SEM-4/ESL/Week4/w4p2.s
@@ -0,0 +1,52 @@
+;hex to packed bcd
+	AREA RESET, DATA, READONLY
+	EXPORT __Vectors
+__Vectors
+	DCD 0X10001000
+	DCD Reset_Handler
+	ALIGN
+	AREA mycode, CODE, READONLY
+	ENTRY
+	EXPORT Reset_Handler
+
+Reset_Handler
+
+	LDR R0, HEX
+	LDR R5, =DST
+	;ADD R5, #3
+	LDR R6, =2	
+	LDR R4, =0
+		
+LOOP
+	CMP R0, #10
+	BCC DIGIT
+	SUB R0, #10
+	ADD R4, #1
+	B LOOP
+
+DIGIT 
+	SUBS R6, #1
+	LSLEQ R0, #4
+	ORREQ R0, R7
+	STRBEQ R0, [R5], #1
+	LDREQ R6, =2
+	MOVNE R7, R0
+	MOV R0, R4
+	LDR R4, =0
+	CMP R0, #0
+	BNE LOOP
+	
+	CMP R6, #0
+	STRBNE R7, [R5], #1
+	
+HERE
+	B HERE
+	
+HEX DCD 0XFFFF
+	AREA mydata, DATA, READWRITE
+DST	DCD 0
+	END
+	
+	
+	
+	
